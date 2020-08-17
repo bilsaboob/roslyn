2871b17a111c6d72d24b214ba0794d1378577f52
bilsaboob <bilsaboob@gmail.com>
2020-08-17 23:48:22 +0200

    Removes irritating asserts ;)
    Boom... this is how you do it when something annoys you ! ;)

--- src/Compilers/CSharp/Portable/Compilation/CSharpCompilation.cs
+++ src/Compilers/CSharp/Portable/Compilation/CSharpCompilation.cs
@@ -933,8 +933,12 @@ namespace Microsoft.CodeAnalysis.CSharp
 
         internal override int GetSyntaxTreeOrdinal(SyntaxTree tree)
         {
-            Debug.Assert(this.ContainsSyntaxTree(tree));
-            return _syntaxAndDeclarations.GetLazyState().OrdinalMap[tree];
+            //Debug.Assert(this.ContainsSyntaxTree(tree) || this.ContainsSyntaxTree(tree?.GetRoot()?.SyntaxTree));
+            var lazyState = _syntaxAndDeclarations.GetLazyState();
+            if (lazyState.OrdinalMap.TryGetValue(tree, out var ordinal)) return ordinal;
+            var root = tree.GetRoot();
+            if (lazyState.OrdinalMap.TryGetValue(root.SyntaxTree, out var rootOrdinal)) return rootOrdinal;
+            return 0;
         }
 
         #endregion
