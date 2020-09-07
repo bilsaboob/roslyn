b6d48af213bc8fbb5d3a29e9f7bc95f792cfe3c7
bilsaboob <bilsaboob@gmail.com>
2020-09-07 23:53:07 +0200

    Adds type helper to DeclarationExpressionSyntax

---
+++ src/Compilers/CSharp/Portable/Syntax/DeclarationExpressionSyntax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis.CSharp.Syntax
+{
+    public partial class DeclarationExpressionSyntax
+    {
+        public bool HasExplicitReturnType()
+        {
+            var noExplicitReturnType = Type.Kind() == SyntaxKind.IdentifierName && Type.Width == 0;
+            return !noExplicitReturnType;
+        }
+    }
+}
