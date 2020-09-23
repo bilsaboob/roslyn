f719b4bd2b090f43298ff5a91f6ec3e4687559bb
bilsaboob <bilsaboob@gmail.com>
2020-09-23 21:41:57 +0200

    Updates binding for inline block statements

--- src/Compilers/CSharp/Portable/Syntax/BlockSyntax.cs
+++ src/Compilers/CSharp/Portable/Syntax/BlockSyntax.cs
@@ -12,6 +12,19 @@ namespace Microsoft.CodeAnalysis.CSharp.Syntax
     {
         public BlockSyntax Update(SyntaxToken openBraceToken, SyntaxList<StatementSyntax> statements, SyntaxToken closeBraceToken)
             => Update(attributeLists: default, openBraceToken, statements, closeBraceToken);
+
+        public bool IsInlineStatement()
+        {
+            return OpenBraceToken.Width == 0 && CloseBraceToken.Width == 0 && Statements.Count == 1;
+        }
+
+        public bool TryGetInlineStatement(out StatementSyntax statement)
+        {
+            statement = null;
+            if (!IsInlineStatement()) return false;
+            statement = Statements[0];
+            return true;
+        }
     }
 }
 
