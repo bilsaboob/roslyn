91d482aa1116b704da71ff154e1cdf0458bb8fcf
bilsaboob <bilsaboob@gmail.com>
2020-11-22 23:57:46 +0100

    Updates class template

--- src/EditorFeatures/RSharp.EditorFeatures.ItemTemplates/Class.cs
+++ src/EditorFeatures/RSharp.EditorFeatures.ItemTemplates/Class.cs
@@ -1,11 +1,10 @@
 ﻿namespace $rootnamespace$
 
-import System;
-import System.Collections.Generic;
+import System
+import System.Collections.Generic
 $if$ ($targetframeworkversion$ >= 3.5)import System.Linq
-$endif$import System.Text;
-$if$ ($targetframeworkversion$ >= 4.5)import System.Threading.Tasks;
+$endif$import System.Text
+$if$ ($targetframeworkversion$ >= 4.5)import System.Threading.Tasks
 $endif$
-
 class $safeitemrootname$ {
 }
