b942244a3e13982391b6572b6032c6d1f4dd26a6
bilsaboob <bilsaboob@gmail.com>
2020-11-22 16:18:13 +0100

    Adds R# class item template project

---
+++ src/EditorFeatures/RSharp.EditorFeatures.ItemTemplates/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace $rootnamespace$
+
+import System;
+import System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)import System.Linq
+$endif$import System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)import System.Threading.Tasks;
+$endif$
+
+class $safeitemrootname$ {
+}
